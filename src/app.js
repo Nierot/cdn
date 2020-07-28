1aaeba2ef4e6c28c4f9634daa99867654fc2abe2
Niels <git@nierot.com>
2020-07-28 12:04:59 +0200

    endpoints

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const app = require('express')();
 const settings = require('../settings.json');
+const files = require('./files');
 
 const PORT = settings.port;
 
 
-app.get('/-/*', (req, res) => {
-    res.send('/-/*');
+app.get(settings.endpoints.standard + "*", (req, res) => {
+    return res.sendFile(`${settings.paths.standard}/${req.url.split('/')[2]}`)
 })
 
-// If all else fails
-app.get('/*', (req, res) => {
-    res.send('fails');
+app.get(settings.endpoints.media + "*", async (req, res) => {
+    return res.sendFile(`${settings.paths.media}/${req.url.split('/')[2]}`)
 })
+
+// app.get('/all', async (req, res) => {
+//     res.json(await files.getAll('/media'));
+// })
+
+app.get('/*', (req, res) => {
+    res.send('<h1>cdn</h1>');
+})
+
+
 
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
