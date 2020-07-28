578a7935fb112d3cbcda3887c769beb9b08eb869
Niels <git@nierot.com>
2020-07-28 12:05:15 +0200

    getAll files from a folder

---
+++ src/files.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+module.exports = {
+    /**
+     * Returns a list of all files
+     */
+    getAll: async path => {
+
+        let list = []
+        await fs.readdir(path, (err, files) => {
+            if (err) console.log(err);
+            files.forEach(file => list.push(file))
+        })
+        await new Promise(r => setTimeout(r, 1)); // Great language
+        return list
+    }
+}
