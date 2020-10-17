ba9f79a2d0600b955d55e814e44632b1f0722024
Nierot <56843328+Nierot@users.noreply.github.com>
2020-10-17 16:03:14 +0200

    added music endpoint

--- src/app.js
+++ src/app.js
@@ -4,38 +4,32 @@ const files = require('./files');
 const serve = require('./serve');
 const dbHelper = require('./mongo');
 const e = require('express');
+const { serveFile, checkExpiration } = require('./serve');
 
 const PORT = settings.port;
 var db = undefined;
 var mongo = undefined;
 
 
-app.get(settings.endpoints.standard + "*", (req, res) => {
-    serve.serveFile(res, `${settings.paths.standard}/${req.url.split('/')[2]}`)
-})
-
-app.get(settings.endpoints.media + "*", (req, res) => {
-    serve.serveFile(res, `${settings.paths.media}/${req.url.split('/')[2]}`)
-})
-
 app.get(settings.endpoints.file_share + "*", async (req, res) => {
-    await serve.checkExpiration(decodeURI(req.url.split(settings.endpoints.file_share)[1].split('/')[0]))
+    await checkExpiration(decodeURI(req.url.split(settings.endpoints.file_share)[1].split('/')[0]))
         .then(() =>
-            serve.serveFile(res, `${settings.paths.file_share}/${decodeURI(req.url.split(settings.endpoints.file_share)[1])}`)
+            serveFile(res, `${settings.paths.file_share}/${decodeURI(req.url.split(settings.endpoints.file_share)[1])}`)
         ).catch(err =>
             res.send('This file has expired, ask the uploader to reupload')
         )
 })
 
-app.get(settings.endpoints.memes + "*", (req, res) => {
-    serve.serveFile(res, `${settings.paths.memes}/${req.url.split('/')[2]}`)
-})
+app.get(settings.endpoints.standard + "*", (req, res) => serveFile(res, `${settings.paths.standard}/${req.url.split('/')[2]}`))
 
-app.get('/*', (req, res) => {
-    res.sendFile('/static/index.html', {
-        root: '.'
-    });
-})
+app.get(settings.endpoints.media + "*", (req, res) => serveFile(res, `${settings.paths.media}/${req.url.split('/')[2]}`))
+
+app.get(settings.endpoints.memes + "*", (req, res) => serveFile(res, `${settings.paths.memes}/${req.url.split('/')[2]}`))
+
+app.get(settings.endpoints.music + "*", (req, res) => serveFile(res, `${settings.paths.music}/${req.url.split('/')[2]}`))
+
+app.get('/*', (req, res) => res.sendFile('/static/index.html', { root: '.' }))
+
 
 app.listen(PORT, async () => {
     let obj = await dbHelper.connect();
